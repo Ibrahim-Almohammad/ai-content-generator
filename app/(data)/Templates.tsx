c7ac0d498ae1906f9fbdd007f2442fcc6b56698f
Ibrahim-m <ibrahim2000almohmd@gmail.com>
2024-07-01 20:09:46 +0300

    i update this code tody

---
+++ app/(data)/Templates.tsx
@@ -0,0 +1,191 @@
+export default[
+    {
+        name:'Code Generator',
+        desc:'An Ai tool that generate a blog title depends on your blog information.',
+        category:'Blog',
+        icon:'/code-generator.svg',
+        aiPrompt:'Give me 5 blog  topic idea bullet wise only on give niche & outline  and give me result in Rich text editor format ',
+        slug:'Code-Generator',
+        form:[
+            {
+                label:'Enter your blog niche',
+                field:'input',
+                name:'Code Generator',
+                required:true
+            },
+            {
+                label:'enter blog outline',
+                field:'textarea',
+                name:'Code Generator',
+            }
+        ]
+    },
+    {
+        name:'Youtube Description',
+        desc:'An Ai tool that service as your personal blog post  title writer,generating catchy ',
+        category:'Blog',
+        icon:'/youtube-color.svg',
+        aiPrompt:'Give me 5 blog  topic idea bullet wise only on give niche & outline  and give me result in Rich text editor format ',
+        slug:'Youtube-Description',
+        form:[
+            {
+                label:'Enter your blog niche',
+                field:'input',
+                name:'Youtube Description',
+                required:true
+            },
+            {
+                label:'enter blog outline',
+                field:'textarea',
+                name:'outline',
+            }
+        ]
+    },
+    {
+        name:'Blog Topic Ideas',
+        desc:'An Ai tool that generate a blog title depends on your blog information.',
+        category:'Blog',
+        icon:'/bulb-creative-idea.svg',
+        aiPrompt:'Give me 5 blog  topic idea bullet wise only on give niche & outline  and give me result in Rich text editor format ',
+        slug:'Blog-Topic-Ideas',
+        form:[
+            {
+                label:'Enter your blog niche',
+                field:'input',
+                name:'Ideas',
+                required:true
+            },
+            {
+                label:'enter blog outline',
+                field:'textarea',
+                name:'outline',
+            }
+        ]
+    },
+    {
+        name:'Instagram Post Generator',
+        desc:'An Ai tool that generate a blog title depends on your blog information.',
+        category:'Blog',
+        icon:'/instagram.svg',
+        aiPrompt:'Give me 5 blog  topic idea bullet wise only on give niche & outline  and give me result in Rich text editor format ',
+        slug:'Instagram-Post-Generator',
+        form:[
+            {
+                label:'Enter your blog niche',
+                field:'input',
+                name:'niche',
+                required:true
+            },
+            {
+                label:'enter blog outline',
+                field:'textarea',
+                name:'outline',
+            }
+        ]
+    },
+    {
+        name:'Youtube Tags',
+        desc:'An Ai tool that generate a blog title depends on your blog information.',
+        category:'Blog',
+        icon:'/video-player.svg',
+        aiPrompt:'Give me 5 blog  topic idea bullet wise only on give niche & outline  and give me result in Rich text editor format ',
+        slug:'Youtube-Tags',
+        form:[
+            {
+                label:'Enter your blog niche',
+                field:'input',
+                name:'niche',
+                required:true
+            },
+            {
+                label:'enter blog outline',
+                field:'textarea',
+                name:'outline',
+            }
+        ]
+    },
+    {
+        name:'Rewrite Article',
+        desc:'An Ai tool that generate a blog title depends on your blog information.',
+        category:'Blog',
+        icon:'/paper-pencil.svg',
+        aiPrompt:'Give me 5 blog  topic idea bullet wise only on give niche & outline  and give me result in Rich text editor format ',
+        slug:'Rewrite-Article',
+        form:[
+            {
+                label:'Enter your blog niche',
+                field:'input',
+                name:'niche',
+                required:true
+            },
+            {
+                label:'enter blog outline',
+                field:'textarea',
+                name:'outline',
+            }
+        ]
+    },
+    {
+        name:'Blog Title',
+        desc:'An Ai tool that generate a blog title depends on your blog information.',
+        category:'Blog',
+        icon:'/blog.svg',
+        aiPrompt:'Give me 5 blog  topic idea bullet wise only on give niche & outline  and give me result in Rich text editor format ',
+        slug:'generate-blog-title',
+        form:[
+            {
+                label:'Enter your blog niche',
+                field:'input',
+                name:'niche',
+                required:true
+            },
+            {
+                label:'enter blog outline',
+                field:'textarea',
+                name:'outline',
+            }
+        ]
+    },
+    {
+        name:'Blog Title',
+        desc:'An Ai tool that generate a blog title depends on your blog information.',
+        category:'Blog',
+        icon:'/blog.svg',
+        aiPrompt:'Give me 5 blog  topic idea bullet wise only on give niche & outline  and give me result in Rich text editor format ',
+        slug:'generate-blog-title',
+        form:[
+            {
+                label:'Enter your blog niche',
+                field:'input',
+                name:'niche',
+                required:true
+            },
+            {
+                label:'enter blog outline',
+                field:'textarea',
+                name:'outline',
+            }
+        ]
+    },
+    {
+        name:'Blog Title',
+        desc:'An Ai tool that generate a blog title depends on your blog information.',
+        category:'Blog',
+        icon:'/blog.svg',
+        aiPrompt:'Give me 5 blog  topic idea bullet wise only on give niche & outline  and give me result in Rich text editor format ',
+        slug:'generate-blog-title',
+        form:[
+            {
+                label:'Enter your blog niche',
+                field:'input',
+                name:'niche',
+                required:true
+            },
+            {
+                label:'enter blog outline',
+                field:'textarea',
+                name:'outline',
+            }
+        ]
+    },
+]
